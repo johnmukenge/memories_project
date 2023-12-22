f7b8855ad6f2db6c167d7fcb6d6314b75c2df404
john.mukenge <mknjhn93r27z312u@sogeiesterni.it>
2023-12-22 17:24:53 +0100

    setting up index.js server side

--- server/index.js
+++ server/index.js
@@ -3,3 +3,10 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+const app = express();
+
+// setting up the bosdy parser to populate the request
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors());
+
